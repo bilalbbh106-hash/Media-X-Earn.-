8e7b344c77bca6842758e67f84a843cfca2e7037
bilalbbh106-hash <bilalbbh106@gmail.com>
2025-12-14 21:40:13 +0100

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,1 @@
+if(CONFIG.ADS_PROVIDER==='adsterra'){let s=document.createElement('script');s.src='https://fedreferawake.com/d7/46/2b/d7462bcbda4583736b172b65f4093349.js';document.body.appendChild(s);}
